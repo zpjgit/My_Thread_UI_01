273f0e4703faa8da499926d4822ce7b3d253eab1
zpjgit <zpj.love@foxmail.com>
2018-09-27 10:41:36 +0800

    android 里确实不允许在子线程里更新UI的更改

--- app/src/main/java/com/example/administrator/my_thread_ui_01/MainActivity.java
+++ app/src/main/java/com/example/administrator/my_thread_ui_01/MainActivity.java
@@ -2,12 +2,39 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+        private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textView1);
+        Button button_1 = (Button) findViewById(R.id.button1);
+        button_1.setOnClickListener(this);
     }
-}
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+
+                case R.id.button1:
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            text.setText("地球，早上好！");
+
+                        }
+                    }).start();
+                    break;
+                 default:
+                     break;
+            }
+        }
+    }
